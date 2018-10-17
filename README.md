0f9f65132f63fe710d00024bd56a77878b83096f
Jan Pospisil <me@jan-pospisil.cz>
2018-10-18 00:48:21 +0200

    Add documentation

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rest server - userlist
+
+## Jak začít
+
+1. Stáhněte si tento balík (git nebo zip)
+2. Nainstalujte si node: [https://nodejs.org/en/](https://nodejs.org/en/)
+3. Nainstalujte si závislosti
+
+```
+$ npm install
+```
+
+3. Nastartujte server
+
+```
+$ npm start
+```
+
+| Endpoint  | method | popis                      |
+| :-------- | :----- | :------------------------- |
+| /user     | get    | Seznam uživatelů           |
+| /user     | post   | Vytvoření nového uživatele |
+| /user/:id | get    | Jeden uživatel             |
+| /user/:id | delete | Smazání uživatele          |
+| /user/:id | post   | Úprava uživatele           |
+
+## Autor
+
+- **Jan Pospíšil** - _programátor a školitel_ - [www.jan-pospisil.cz](https://www.jan-pospisil.cz)
--- src/index.js
+++ src/index.js
@@ -6,30 +6,33 @@ const removeUserController = require("./user/removeUserController");
 const createUserController = require("./user/addUserController");
 const updateUserController = require("./user/updateUserController");
 const userController = require("./user/userController");
-
 const restifyBodyParser = require("restify-plugins").bodyParser;
 const corsMiddleware = require("restify-cors-middleware");
 
+// create server
 const server = restify.createServer();
 
+// cors
 const cors = corsMiddleware({
   preflightMaxAge: 5,
   origins: ["http://localhost:3000"],
   allowHeaders: ["API-Token"],
   exposeHeaders: ["API-Token-Expiry"]
 });
-
 server.pre(cors.preflight);
 server.use(cors.actual);
 
+// body parser for post data
 server.use(restifyBodyParser());
 
+// routes
 server.get("/user", userListController);
 server.get("/user/:id", userController);
 server.del("/user/:id", removeUserController);
 server.post("/user/:id", updateUserController);
 server.post("/user", createUserController);
 
+// startup
 server.listen(8888, function() {
   console.log("%s listening at %s", server.name, server.url);
 });
